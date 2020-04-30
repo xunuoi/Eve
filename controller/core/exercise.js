d2072473ccf7b614af9ed31cfb44a0da3e710582
Prism <xwlxyjk@gmail.com>
2020-04-30 23:33:40 +0800

    Update exercise.js

--- controller/core/exercise.js
+++ controller/core/exercise.js
@@ -14,19 +14,6 @@ function doEx(){
     eve.learn('tell me your age::0.1');
     // eve.learn('your age::0.1');
     eve.learn('how old are you::I am 0.1');
-    //dirty
-    eve.learn('fuck you::fuck your self!');
-    eve.learn('fuck::come on');
-    eve.learn('fuck u::fuck your self!');
-    eve.learn('kill you::kill your mother!');
-    eve.learn('you sb::you too :（');
-    eve.learn('you are shit::i shit you');
-    eve.learn('you are sb::you too :（');
-
-    eve.learn('i want kill sb::kill ni mei a !sb');
-    eve.learn('i want kiss you::love you');
-    eve.learn('do you want eat shit::yes, i love shit');
-    eve.learn('do you want eat apple::apple your sister');
 }
 
 exports.init = function(eveMod){
